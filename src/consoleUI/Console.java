be1064f0dba99ac7a942e7915bd40c7c1d69ded2
BA201 <BA201@MCBA201-C24.classrooms.stlcc>
2017-02-15 13:23:59 -0600

    First Commit

--- src/consoleUI/Console.java
+++ src/consoleUI/Console.java
@@ -4,7 +4,9 @@ package consoleUI;
 public class Console {
 
     public static void main (String[] args){
-        System.out.println("Hello Github!");
+    System.out.println("Hello Github!");
+    System.out.println("yaaay Github! rock on");    
+        
         }
     }
 
